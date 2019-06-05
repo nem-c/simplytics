2904356011aa3d02e58f4762501dec847fb6031c
Nem C <nem-c@users.noreply.github.com>
2019-06-05 16:46:25 +0200

    remove domain from configuration

--- src/scripts/sl.js
+++ src/scripts/sl.js
@@ -102,17 +102,17 @@ var Base64 = {
     imE.src = imD + '?' + imQS;
 
     function cb() {
+        var d = p(window.location.href);
         u = {};
         u.p = '';
         u.s = '//'
-        u.pf = 'www.';
-        u.d = 'globalauctionguide';
-        u.e = '.com';
+        u.pf = d.subdomain + '.';
+        u.d = d.host;
+        u.e = '.' + d.tld;
         u.u = '/simplytics/track/';
 
         if (iSN()) {
-            u.d = 'global';
-            u.e = '.dev';
+            u.e = '.local';
         }
         if (iSD()) {
             u.pf = 'dev.';
@@ -238,6 +238,45 @@ var Base64 = {
             m += p.charAt(Math.floor(Math.random() * p.length));
 
         return m;
+    }
+
+    function p(url){
+        parsed_url = {}
+
+        if ( url == null || url.length == 0 )
+            return parsed_url;
+
+        protocol_i = url.indexOf('://');
+        parsed_url.protocol = url.substr(0,protocol_i);
+
+        remaining_url = url.substr(protocol_i + 3, url.length);
+        domain_i = remaining_url.indexOf('/');
+        domain_i = domain_i == -1 ? remaining_url.length - 1 : domain_i;
+        parsed_url.domain = remaining_url.substr(0, domain_i);
+        parsed_url.path = domain_i == -1 || domain_i + 1 == remaining_url.length ? null : remaining_url.substr(domain_i + 1, remaining_url.length);
+
+        domain_parts = parsed_url.domain.split('.');
+        switch ( domain_parts.length ){
+            case 2:
+                parsed_url.subdomain = null;
+                parsed_url.host = domain_parts[0];
+                parsed_url.tld = domain_parts[1];
+                break;
+            case 3:
+                parsed_url.subdomain = domain_parts[0];
+                parsed_url.host = domain_parts[1];
+                parsed_url.tld = domain_parts[2];
+                break;
+            case 4:
+                parsed_url.subdomain = domain_parts[0];
+                parsed_url.host = domain_parts[1];
+                parsed_url.tld = domain_parts[2] + '.' + domain_parts[3];
+                break;
+        }
+
+        parsed_url.parent_domain = parsed_url.host + '.' + parsed_url.tld;
+
+        return parsed_url;
     }
 
 })(_sl);
